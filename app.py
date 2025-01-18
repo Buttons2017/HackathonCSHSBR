4aae354fd9b6e1fc09aba91c8e7eaa219f5424b6
TheFastestFox <143468459+TheFastestFox@users.noreply.github.com>
2025-01-18 15:43:24 -0600

    Final files!!

--- app.py
+++ app.py
@@ -29,15 +29,19 @@ shower = 0.0
 sink = 0.0
 toilet = 0.0
 totalKWH = 0.0
+totalKWHC = 0.0
 totalGallons = 0.0
 totalWaterCost = 0.0
+feedback = ''
+
 
 # Define routes
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global counter, lightCount, lightHours, kWHlights, dishwasher, tv, oven, fridge, dryer, laundry, centerAC, \
-        windowCount, windowTime, windowCost, shower, sink, toilet, totalKWH, totalKWHC, totalGallons, totalWaterCost
+        windowCount, windowTime, windowCost, shower, sink, toilet, totalKWH, totalKWHC, totalGallons, totalWaterCost,\
+        feedback
     if request.method == 'POST':
         # Check which form was submitted
         if 'increment_button' in request.form:
@@ -77,10 +81,45 @@ def index():
         totalGallons = round(shower+sink+toilet,1)
         totalWaterCost = round(totalGallons*(5/750),2)
 
+        feedback = ''
+
+        # Personalized feedback
+        if float(lightHours) >= 12:
+            feedback += 'Consider turning off your lights at night and when you aren\'t at home.\n'
+        if float(dishwasher) >= 3:
+            feedback += 'Try doing one load of dishes a day by waiting until your dishwasher is full to run it.\n'
+
+        if shower*(60/150) >= 2.5*60:
+            feedback += 'Try to take quicker showers to conserve water, and encourage your household to do the same.\n'
+
+        if sink*(60/200) > 60:
+            feedback += "Make sure you don't leave your sink running when you aren't using it.\n"
+
+        if centerAC/3.5 >= 22:
+            feedback += 'Consider turning off your central AC when you aren\'t at home, or otherwise don\'t need it on.\n'
+
+        if laundry*(7/1.5) >= 10:
+            feedback += 'Try to only run loads of laundry when the washing machine is full.\n'
+
+        if dryer*(7/4.5) >= 15:
+            feedback += 'Try to only run the drying machine when it\'s full.\n'
+
+        if float(windowTime) >= 16:
+            feedback += 'Consider turning off window AC units when they aren\'t needed.\n'
+
+        if tv/0.05 >= 8:
+            feedback += 'Please consider turning the TV off, going outside, and touching grass.'
+
+        if toilet/0.47 >= 100:
+            feedback += 'what are you doing to that toilet.'
+
+
+
     return render_template('index2.html', counter=counter, lightCount=lightCount, lightHours=lightHours,
                            kWHlights=kWHlights, dishwasher=dishwasher, tv=tv, oven=oven, fridge=fridge, laundry=laundry,
                            dryer=dryer, centerAC=centerAC, windowCost=windowCost, shower=shower, sink=sink, toilet=toilet,
-                           totalKWH=totalKWH, totalKWHC=totalKWHC, totalGallons=totalGallons, totalWaterCost=totalWaterCost)
+                           totalKWH=totalKWH, totalKWHC=totalKWHC, totalGallons=totalGallons, totalWaterCost=totalWaterCost,
+                           feedback=feedback)
 
 
 @app.route('/test')
