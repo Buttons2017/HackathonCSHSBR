0940164746a9807c7847e402bd28c9f3f003fd8c
TheFastestFox <143468459+TheFastestFox@users.noreply.github.com>
2025-01-18 12:55:52 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -18,12 +18,24 @@ kWHlights = 0.0
 dishwasher = 0.0
 tv = 0.0
 oven = 0.0
+fridge = 0.0
+laundry=0.0
+dryer=0.0
+centerAC = 0.0
+windowCount = 0.0
+windowTime = 0.0
+windowCost = 0.0
+shower = 0.0
+sink = 0.0
+toilet = 0.0
+totalKWH = 0.0
 
 # Define routes
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global counter, lightCount, lightHours, kWHlights, dishwasher, tv, oven
+    global counter, lightCount, lightHours, kWHlights, dishwasher, tv, oven, fridge, dryer, laundry, centerAC, \
+        windowCount, windowTime, windowCost, shower, sink, toilet, totalKWH, totalKWHC
     if request.method == 'POST':
         # Check which form was submitted
         if 'increment_button' in request.form:
@@ -39,16 +51,31 @@ def index():
         elif 'oven' in request.form:
             oven = round(4*float(request.form.get('oven', '')),1)
         elif 'fridge' in request.form:
-            fridge = round(0.05 * float(request.form.get('fridge', '')), 1)
+            fridge = round(0.05*24 * float(request.form.get('fridge', '')), 1)
         elif 'laundry' in request.form:
-            laundry = round(1.5 * float(request.form.get('laundry', '')), 1)
+            laundry = round(1.5/7 * float(request.form.get('laundry', '')), 1)
         elif 'dryer' in request.form:
-            dryer = round(4.5 * float(request.form.get('dryer', '')), 1)
+            dryer = round(4.5/7 * float(request.form.get('dryer', '')), 1)
+        elif 'centerAC' in request.form:
+            centerAC = round(3.5*float(request.form.get('centerAC', '')))
+        elif 'windowCount' in request.form:
+            windowCount = int(request.form.get('windowCount', ''))
+            windowTime = float(request.form.get('windowHours', ''))
+            windowCost = round(windowCount * windowTime * 0.618,1)
+        elif 'shower' in request.form:
+            shower = round((150/60)*int(request.form.get('shower')),1)
+        elif 'sink' in request.form:
+            sink = round((200/60)*int(request.form.get('sink')),1)
+        elif 'toilet' in request.form:
+            toilet = round(0.47*int(request.form.get('toilet')),1)
 
-
+        totalKWH = round(kWHlights+dishwasher+tv+oven+fridge+laundry+dryer+centerAC+windowCost+shower+sink+toilet,1)
+        totalKWHC = round(totalKWH*0.17,2)
 
     return render_template('index2.html', counter=counter, lightCount=lightCount, lightHours=lightHours,
-                           kWHlights=kWHlights, dishwasher=dishwasher, tv=tv, oven=oven)
+                           kWHlights=kWHlights, dishwasher=dishwasher, tv=tv, oven=oven, fridge=fridge, laundry=laundry,
+                           dryer=dryer, centerAC=centerAC, windowCost=windowCost, shower=shower, sink=sink, toilet=toilet,
+                           totalKWH=totalKWH, totalKWHC=totalKWHC)
 
 
 @app.route('/test')
